01c8f13fa122440da0a95c3f63c48c930ad1dc99
leonardo <leonardo.novelletto.grieshaber@gmail.com>
2026-05-22 20:36:20 -0300

    feat:Cria as classes AnimalDAO e TelaCadastroAnimal

---
+++ src/AnimalDAO.java
@@ -0,0 +1,33 @@
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.swing.JOptionPane;
+
+import com.mysql.cj.xdevapi.PreparableStatement;
+
+public class AnimalDAO {
+	
+	
+	public void Salvar(Animal animal) {	
+		String sql = "INSERT INTO Animal(nome , especie , peso) VALUES (? , ? , ?)";
+		try {
+			BD bd = new BD();
+			bd.getConnection();
+			PreparedStatement stmt = bd.connection.prepareStatement(sql);			
+			
+			stmt.setString(1 , animal.getNome());
+			stmt.setString(2 , animal.getEspecie().toString());
+			stmt.setDouble(3 , animal.getPeso());
+			
+			stmt.execute();
+			stmt.close();
+			bd.close();
+			
+			JOptionPane.showMessageDialog(null, "Animal Cadastrado");
+		} catch (SQLException e) {
+			
+		}
+	}
+	
+	
+}
